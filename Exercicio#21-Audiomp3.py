855611b5ff875967781127692172e5398e90ca1c
CassioMaciel <cassio.mac.eng@gmail.com>
2018-07-12 17:39:51 -0300

    Backup

---
+++ Exercicio#21-Audiomp3.py
@@ -0,0 +1,6 @@
+##########################	Enunciado	########################################
+#	Faça um programa que toque um audio mp3
+#
+################################################################################
+import libvlc
+#webbrowser.open("./R.E.M. - Losing My Religion (Video).mp3")
